0d4f41b72fbf877e5cb5f6edbf69b89bb1604ec0
ali <heunle23@gmail.com>
2024-10-06 00:42:32 +0330

    ✨ add "accepted_char" and  "hashfunc" funcs , they need to be inside SymbolTable  class .

--- Ali.cpp
+++ Ali.cpp
@@ -33,6 +33,34 @@ public:
 
 
 
+void accepted_char(){
+    for (int i = 48; i < 123; ++i) {
+        if ((i>=58 && i<=64) || (i>=91 && i<=96)){}
+        else{
+            char c = (char)i;
+            cout << i << ": " << c << std::endl;
+            }
+        if (i == 95)
+        {
+            char c = (char)i;
+            cout << i << ": " << c << std::endl;
+        }
+    }
+}
+
+//Horner's Rule
+unsigned int hashfunc(const string& identifier) {
+    unsigned int hash_value = 0;
+    const int base = 31; // Prime number for better distribution
+
+    for (char c : identifier) {
+        hash_value = hash_value * base + c;
+    }
+
+    return hash_value % 100; // Modulo 100 to fit within the array size
+}
+
+
 int main(){
 
     SymbolTable st;
