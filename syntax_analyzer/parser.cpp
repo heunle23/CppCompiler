de75d07663da2dbfbba574d84d5a4b0df5c345b9
ali <heunle23@gmail.com>
2024-12-10 13:45:05 +0330

    completing code

--- syntax_analyzer/parser.cpp
+++ syntax_analyzer/parser.cpp
@@ -20,7 +20,7 @@ class RDFH{
     
     Token lexitem;
     LexicalAnalyzer lexianalyzer;
-    int error = 0;
+    int error_count  = 0;
 
     void scanner(){
         lexitem = lexianalyzer.touch(); 
@@ -37,20 +37,20 @@ class RDFH{
         scanner(); // تنها باری که اسکنر صدا زده میشود ولی قبلش مقایسه رخ نداده
 
         if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "PROGRAM")){
-            error +=1 ;
+            error_count +=1 ;
             cerr << "error at line = " << lexitem.line << endl << "should use PROGRAM keyword first " << endl;
         }
         scanner();
         
         if ( !(lexitem.type == TokenType::IDENTIFIER )){
-            error +=1 ;
+            error_count +=1 ;
             cerr << "error at line = " << lexitem.line << endl << "should use IDENTIFIER " << endl;
         }
 
         scanner();
 
         if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ";" )){
-            error +=1;
+            error_count +=1;
             cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
         }
 
@@ -59,12 +59,12 @@ class RDFH{
         ParseBlock();
 
         if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == "." )){
-            error +=1;
+            error_count +=1;
             cerr << "error at line = " << lexitem.line << endl << "should use period " << endl;
         }
 
 
-        if (error == 0){
+        if (error_count == 0){
             // accpted 
         }
         else{
@@ -76,11 +76,158 @@ class RDFH{
 
     void ParseBlock(){
 
-        
+        if (lexitem.type == TokenType::KEYWORD && lexitem.value == "VAR"){
+            scanner;
+            ParseVarList();
+        }
+        while (lexitem.type == TokenType::KEYWORD && lexitem.value == "FUNCTION")
+        {
+            scanner;
+            ParseFunctionDefinition();
+        }
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "BEGIN ")){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use BEGIN " << endl;
+        }
+        ParseFunctionBody();
+
+    }
+
+    void ParseVarList(){
+        ParseDataDefinition();
+
+        while (lexitem.type == TokenType::IDENTIFIER){
+            scanner;
+            ParseDataDefinition();
+        }
 
 
     }
 
+    void ParseDataDefinition(){
+        ParseIdentifierList();
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ":" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use colon " << endl;
+        }
+        scanner();
+
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "INTEGER" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use INTEGER keyword " << endl;
+        }
+        scanner();
+
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ";" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+        }
+        scanner();
+
+
+    }
+
+    void ParseIdentifierList(){
+
+        if ( !(lexitem.type == TokenType::IDENTIFIER )){
+            error_count +=1 ;
+            cerr << "error at line = " << lexitem.line << endl << "should use IDENTIFIER " << endl;
+        }
+        scanner();
+
+        // below code needs to be tested
+        // اگر منطق پایین اوک بود بقیه حلقه ها هم همین طوری شوند
+        while (lexitem.value.length() == 1){
+            
+            if ( lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ","){
+            error_count +=1 ;
+            cerr << "error at line = " << lexitem.line << endl << "should use IDENTIFIER " << endl;
+            }
+    
+            scanner();
+
+            if ( !(lexitem.type == TokenType::IDENTIFIER )){
+            error_count +=1 ;
+            cerr << "error at line = " << lexitem.line << endl << "should use IDENTIFIER " << endl;
+
+            }
+            scanner();
+
+        }
+
+
+
+    }
+
+
+    void ParseFunctionDefinition(){
+        ParseHeading();
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ";" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+        }
+        scanner();
+
+        ParseBlock();
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ";" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+        }
+        scanner();
+
+
+
+    }
+
+    void ParseHeading(){
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "FUNCTION" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use FUNCTION keyword " << endl;
+        }
+        scanner();
+
+        if ( !(lexitem.type == TokenType::IDENTIFIER )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use identifier " << endl;
+        }
+        scanner();
+
+
+        if (lexitem.type == TokenType::PUNCTUATOR && lexitem.value == "(" ){
+            ParseParameterList();
+        }
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ":" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+        }
+        scanner();
+
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "INTEGER" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use INTEGER keyword " << endl;
+        }
+        scanner();
+
+
+
+    }
+    
+    void ParseParameterList(){
+
+
+
+
+    }
+
+    void ParseFunctionBody(){}
+
+    
+    
 
 
 
