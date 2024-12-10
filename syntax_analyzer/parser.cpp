30cb61080c7dac102beb625f10c4fe1b4c8016ff
ali <heunle23@gmail.com>
2024-12-10 14:24:11 +0330

    🚧  finished BUT needs testing

--- syntax_analyzer/parser.cpp
+++ syntax_analyzer/parser.cpp
@@ -85,10 +85,6 @@ class RDFH{
             scanner;
             ParseFunctionDefinition();
         }
-        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "BEGIN ")){
-            error_count +=1;
-            cerr << "error at line = " << lexitem.line << endl << "should use BEGIN " << endl;
-        }
         ParseFunctionBody();
 
     }
@@ -139,7 +135,7 @@ class RDFH{
 
         // below code needs to be tested
         // اگر منطق پایین اوک بود بقیه حلقه ها هم همین طوری شوند
-        while (lexitem.value.length() == 1){
+        while ( lexitem.value.length() == 1 && !(lexitem.value == ":")){
             
             if ( lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ","){
             error_count +=1 ;
@@ -219,16 +215,185 @@ class RDFH{
     
     void ParseParameterList(){
 
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == "(" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use opening parenthesis " << endl;
+        }
+        scanner();
+
+        ParseIdentifierList();
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ":" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use opening parenthesis " << endl;
+        }
+        scanner();
+
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "INTEGER" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use INTEGER keyword " << endl;
+        }
+        scanner();
+
+        if ( !(lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ")" )){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use closing parenthesis " << endl;
+        }
+        scanner();
 
 
 
     }
 
-    void ParseFunctionBody(){}
+    void ParseFunctionBody(){
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "BEGIN ")){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use BEGIN keyword" << endl;
+        }
+        scanner();
+
+        ParseStatementSequence();
+
+        if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "END ")){
+            error_count +=1;
+            cerr << "error at line = " << lexitem.line << endl << "should use END keyword " << endl;
+        }
+        scanner();
+
+
+    }
 
     
-    
+    void ParseStatementSequence(){
+        ParseStatment();
+
+        while (lexitem.value.length() == 1 ){
+            
+            if ( lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ";"){
+            error_count +=1 ;
+            cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+            }
+            scanner();
+
+            ParseStatment();
+
+            
+
+        }
 
 
+
+
+    }
+
+    void ParseStatment()
+    {
+
+        if (lexitem.type == TokenType:: KEYWORD)
+        {
+            if (lexitem.value == "BEGIN")
+            {
+                scanner();
+                ParseStatementSequence();
+
+                if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "END "))
+                {
+                    error_count +=1;
+                    cerr << "error at line = " << lexitem.line << endl << "should use END keyword " << endl;
+                }
+                scanner();
+
+            }
+            else if (lexitem.value == "IF")
+            {
+                scanner();
+                ParseExpression();
+
+                if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "THEN "))
+                {
+                    error_count +=1;
+                    cerr << "error at line = " << lexitem.line << endl << "should use THEN keyword " << endl;
+                }
+                scanner();
+
+                ParseStatment();
+
+                if (lexitem.type == TokenType::KEYWORD && lexitem.value == "ELSE ")
+                {
+                    scanner();
+                    ParseStatment();
+                    
+                }
+                
+
+
+            }
+            
+            else if(lexitem.value == "WHILE")
+            {
+                scanner();
+                ParseExpression();
+
+                if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "DO "))
+                {
+                    error_count +=1;
+                    cerr << "error at line = " << lexitem.line << endl << "should use DO keyword " << endl;
+                }
+                scanner();
+                ParseStatment();
+
+
+            }
+            else if(lexitem.value == "FOR")
+            {
+                scanner();
+
+                if ( !(lexitem.type == TokenType::IDENTIFIER ))
+                {
+                    error_count +=1;
+                    cerr << "error at line = " << lexitem.line << endl << "should use identifier " << endl;
+                }
+                scanner();
+
+                if ( lexitem.type == TokenType::PUNCTUATOR && lexitem.value == ":=")
+                {
+                    error_count +=1 ;
+                    cerr << "error at line = " << lexitem.line << endl << "should use semicolon " << endl;
+                }
+                scanner();
+                ParseExpression();
+
+                if ( lexitem.type == TokenType::KEYWORD && (lexitem.value == "TO" || lexitem.value == "DOWN TO" ))
+                {
+                    error_count +=1 ;
+                    cerr << "error at line = " << lexitem.line << endl << "should use DOWN or  DOWN TO " << endl;
+                }
+                scanner();
+                ParseExpression();
+
+                if ( !(lexitem.type == TokenType::KEYWORD && lexitem.value == "DO "))
+                {
+                    error_count +=1;
+                    cerr << "error at line = " << lexitem.line << endl << "should use DO keyword " << endl;
+                }
+                scanner();
+                ParseStatment();
+
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+    void ParseExpression();
 
 };
